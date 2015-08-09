973c80b0972bb4f250b6d113016c435e7a328f5b
Dmitry Kalinin <null.none@yandex.ru>
2015-08-10 00:11:28 +0300

    Backup script

---
+++ script.py
@@ -0,0 +1,26 @@
+from flask.ext.script import Manager, Command
+from flask import Flask
+
+import yandexwebdav
+import time
+import os
+
+app = Flask(__name__)
+manager = Manager(app)
+
+
+@manager.command
+def backup():
+    conf = yandexwebdav.Config({
+        "user": "user",
+        "password": "pass"
+        })
+
+    directory = "/PATH/"
+
+    for filename in os.listdir(directory):
+        current_file = "{0}{1}".format(directory, filename)
+        if time.time() - os.path.getmtime(current_file) > (21 * 24 * 60 * 60):
+            conf.upload(current_file,
+                        "/PATH/{0}".format(filename))
+            os.remove(current_file)
